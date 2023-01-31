237922126c5c847f045590faa851c5a286f6ecd9
monisj <koolmonis@gmail.com>
2023-01-31 19:40:19 +0500

    Added Changable Video Resolutions

--- video.py
+++ video.py
@@ -12,5 +12,8 @@ except:
 import pytube
 link = input("Enter URL=")
 video=pytube.YouTube(link)
-video.streams.first().download()
+#video.streams.first().download() #This controls the video quality of the stream
+video.streams.filter(progressive=True,file_extension='mp4')#Use this to further filter out the video
+video.streams.get_by_resolution("720p").download()
+# See the url https://pytube.io/en/latest/api.html#stream-object for assitance
 print("Download Link =",link)
